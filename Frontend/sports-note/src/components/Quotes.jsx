b460e63e39965a1bbd41adae2c7701b5e8ab99a5
Mohamed Shakin <shakincc02@gmail.com>
2025-11-16 00:15:03 +0400

    added quotes page

---
+++ Frontend/sports-note/src/components/Quotes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Quotes() {
+  return (
+    <section className="quotes-section container-md py-5 mt-2 px-2">
+      <div className="heading-container mb-5">
+        <h1 className="m-0 p-0 mb-3">Quotes</h1>
+        <p className="m-0 p-0 fs-4 w-75">
+          Read your favorite quotes from players and coaches. Let their words inspire and motivate you every day.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Quotes
--- Frontend/sports-note/src/pages/QuotesPage.jsx
+++ Frontend/sports-note/src/pages/QuotesPage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Header from '../components/header'
+import Quotes from '../components/Quotes'
 
 function QuotesPage() {
   return (
-    <div>QuotesPage</div>
+    <>
+    <style>
+        {`
+          body {
+            background-color: #710004; 
+          }
+        `}
+      </style>
+
+      <main>
+        <Header/>
+        <Quotes/>
+      </main>
+    </>
   )
 }
 
