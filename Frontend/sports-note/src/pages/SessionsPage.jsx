7970756472e06400cd041041709eba8708d4bf8c
Mohamed Shakin <shakincc02@gmail.com>
2025-11-16 18:51:57 +0400

    created sessions page

--- Frontend/sports-note/src/pages/SessionsPage.jsx
+++ Frontend/sports-note/src/pages/SessionsPage.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import Sessions from '../components/Sessions'
+import Header from '../components/header'
 
 function SessionsPage() {
   return (
-    <div>SessionsPage</div>
+    <>
+    <style>
+        {`
+          body {
+            background-color: #710004; 
+          }
+
+          header{
+            background-color: #FCF0D6;
+          }
+
+          .logo p a, .menu-links li a, .Logo a{
+            color: black !important
+          }
+
+          body .menu-links li a:hover {
+            color: #710004 !important;
+          }
+
+          .auth-dropdown i, .bi-grid-fill, .menu-links li hr{
+           filter: invert(100%);
+          }
+        `}
+      </style>
+
+      <main>
+        <Header/>
+        <Sessions/>
+      </main>
+    </>
   )
 }
 
