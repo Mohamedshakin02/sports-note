7a6205280becc21e53abd336c0391adef74773ca
Mohamed Shakin <shakincc02@gmail.com>
2025-12-11 22:49:27 +0400

    removed local server link

--- Backend/app.js
+++ Backend/app.js
@@ -16,10 +16,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://sportsnote.vercel.app"],
+  origin: ["https://sportsnote.vercel.app"],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json());
