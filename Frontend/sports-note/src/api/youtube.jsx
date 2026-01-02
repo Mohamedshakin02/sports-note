b8b90bdc2a68c31742f99727796660cae38b6a17
Mohamed Shakin <shakincc02@gmail.com>
2026-01-03 00:27:25 +0400

    added comments in frontend

--- Frontend/sports-note/src/api/youtube.jsx
+++ Frontend/sports-note/src/api/youtube.jsx
@@ -1,8 +1,13 @@
+// Function to fetch YouTube videos based on a given title query
+
 export const fetchVideos = async (query) => {
   try {
+    // Sends a GET request to the backend
     const res = await fetch(`https://sports-note-backend.onrender.com/api/youtube?q=${encodeURIComponent(query)}`);
     if (!res.ok) throw new Error("Failed to fetch videos");
     const data = await res.json();
+
+    // Returns the fetched video data
     return data;
   } catch (err) {
     console.error("Error fetching videos:", err);
