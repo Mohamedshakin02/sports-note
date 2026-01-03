85f47b296349542067dcea059ef2f739f368ee03
Mohamed Shakin <shakincc02@gmail.com>
2026-01-04 00:20:50 +0400

    Updated the year in copyright in footer

--- Frontend/sports-note/src/components/layout/Footer.jsx
+++ Frontend/sports-note/src/components/layout/Footer.jsx
@@ -66,7 +66,7 @@ function Footer() {
             </div>
 
             <div className='copyright mt-5 mt-lg-3'>
-                <p className='m-0 p-0'>&copy; 2025 SPORTS NOTE</p>
+                <p className='m-0 p-0'>&copy; 2026 SPORTS NOTE</p>
             </div>
         </footer>
     );
